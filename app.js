ee50f1fe249abd7ab4688ee2a7684dae4ca9dab7
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-03 18:31:00 -0500

    Loop, render and append mushrooms

--- app.js
+++ app.js
@@ -67,9 +67,13 @@ function displayFriends() {
 
 function displayMushrooms() {
     // clear out the mushroom div
+    mushroomsEl.textContent = '';
 
     for (let i = 0; i < mushroomCount; i++) {
         // for each mushroom in your mushroom state, render and append a mushroom
+        const mushroomEl = renderMushroom();
+
+        mushroomsEl.append(mushroomEl);
     }
 }
 
